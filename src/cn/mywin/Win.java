f1aa9a34cfa42128b6bc8fc1524a3acfe545c3c1
wyj <3048155352@qq.com>
2017-12-24 14:56:49 +0800

    12人人基本完成

---
+++ src/cn/mywin/Win.java
@@ -0,0 +1,202 @@
+package cn.mywin;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by wyj on 2017/12/14.
+ */
+public class Win {
+    String  sessionid;
+    String  message;
+    String esessionid;
+    //连接本地的 Redis 服务
+    Jedis jedis = new Jedis("localhost");
+    public  Win(String sessionid,String esessionid, String message){
+           this.sessionid=sessionid;
+           this.message=message;
+           this.esessionid=esessionid;
+
+        if(message.indexOf("&")!=-1){   //判断消息中是否含有&
+            System.out.println("包含");
+        }
+        else
+    if(message.contains("!"))
+        {
+             String[] str=message.split("!");
+             message=str[1];
+
+
+            jedis.sadd("!"+sessionid, message);
+            if (jedis.smembers("!"+esessionid).isEmpty()){
+                jedis.set("#"+sessionid,"true");
+              //  System.out.println("敌方集合是空的");
+            }
+
+
+
+            //jedis.sadd("!"+esessionid, message);
+
+            System.out.println("不包含");
+            System.out.println("测试:"+message);
+            System.out.println("获取到的坐标:"+message);
+
+
+
+        }
+
+
+    }
+public String cal(String message) {
+
+        boolean isblack=false;
+    if(message.substring(0,1).equals("b")){
+
+       isblack=true;
+    }
+    String[] strs=message.split("\\.");
+    System.out.println("截取的长度："+strs.length);
+    int num1=Integer.parseInt(strs[1]);
+
+    int num2=Integer.parseInt(strs[2]);
+
+
+
+    System.out.println("执行了");
+    Set<String> set = jedis.smembers("!"+sessionid);
+    Iterator<String> it=set.iterator() ;
+
+//初始化棋盘数组
+
+    int chessData[][]=new int[15][15];
+    for (int x = 0; x < 15; x++) {
+
+        for (int y = 0; y < 15; y++) {
+            chessData[x][y] = 0;
+        }
+
+    }
+    while(it.hasNext()){
+        String obj=it.next();
+       String[] str=obj.split(",");
+
+       System.out.println("坐标："+str[0]+str[1]);
+        int numa1 = Integer.parseInt(str[0]);
+        int numa2= Integer.parseInt(str[1]);
+
+        chessData[numa1][numa2]=1;
+
+    }
+
+    int hz = 0;
+    int ve = 0;
+    int ls = 0;
+    int rs = 0;
+      for(int i = num1; i > 0; i--){
+          if (chessData[i][num2] != 1) {//判断右边是否有其他的颜色棋子
+
+              break;
+          }
+
+          hz++;
+      }
+    for (int i = num1 + 1; i < 15; i++) {
+        if (chessData[i][num2] != 1) {
+            break;
+        }
+        hz++;
+    }
+    for (int i = num2; i > 0; i--) {      // //计算上下方向的输赢
+        if (chessData[num1][i] != 1) {
+            break;
+        }
+        ve++;
+    }
+    for (int i = num2 + 1; i < 16; i++) {
+        if (chessData[num1][i] != 1) {
+            break;
+        }
+        ve++;
+    }
+    for (int i = num1, j = num2; i > 0&& j > 0; i--, j--) {   //判断左上右下的棋子
+        if (chessData[i][j] != 1) {
+            break;
+        }
+        ls++;
+    }
+    for (int i = num1 + 1, j = num2 + 1; i < 15&& j < 15; i++, j++) {
+        if (chessData[i][j] != 1) {
+            break;
+        }
+        ls++;
+    }
+
+    for (int i = num1, j = num2; i < 15&&j > 0; i++, j--) {
+        if (chessData[i][j] != 1) {
+            break;
+        }
+        rs++;
+    }
+    for (int i = num1 - 1, j = num2 + 1; i > 0&&j < 15; i--, j++) {
+        if (chessData[i][j] != 1) {
+            break;
+        }
+        rs++;
+    }
+
+
+  if (hz==5||ve==5||ls==5||rs==5){
+
+
+      System.out.println("交集："+jedis.sinter("!"+sessionid,"!"+ esessionid).isEmpty());
+
+          if (isblack) {
+
+
+             message="@3";
+          }else{
+              message="@4";
+          }
+          if (!jedis.sinter("!"+sessionid,"!"+ esessionid).isEmpty()){
+              message="@5";   //非法操作
+
+          }
+      Set<String> set1 = jedis.smembers("!"+esessionid);
+      Iterator<String> it1=set1.iterator() ;
+      Set<String> set2 = jedis.smembers("!"+sessionid);
+      Iterator<String> it2=set2.iterator() ;
+
+
+      while(it2.hasNext()){
+          String obj=it2.next();
+          jedis.srem("!"+sessionid, obj);
+
+      }
+      while(it1.hasNext()){
+          String obj1=it1.next();
+          jedis.srem("!"+esessionid, obj1);
+
+      }
+      System.out.println("查看sets1集合中的所有元素:"+jedis.smembers("!"+sessionid));
+      System.out.println("查看sets2集合中的所有元素:"+jedis.smembers("!"+ esessionid));
+  }
+
+    return message;
+    }
+    public String undo(String message) {
+        String[] str=message.split("#");
+        message=str[1];
+        System.out.println("判断element001是否在集合sets中："+jedis.sismember("!"+sessionid, message));
+        if (jedis.sismember("!"+sessionid, message)){
+            jedis.srem("!"+sessionid, message);
+        }
+        if (jedis.sismember("!"+esessionid, message)){
+            jedis.srem("!"+esessionid, message);
+        }
+        return  "good";
+
+    }
+
+}
--- src/mywebsocket/WebSocketTest.java
+++ src/mywebsocket/WebSocketTest.java
@@ -1,13 +1,16 @@
 package mywebsocket;
 
 import cn.configure.GetHttpSessionConfigurator;
+import cn.mywin.Win;
 import redis.clients.jedis.Jedis;
 
 import javax.servlet.http.HttpSession;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 /**
  * @ServerEndpoint 注解是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端,
@@ -49,9 +52,11 @@ public class WebSocketTest {
      */
     @OnOpen
     public void onOpen(Session session, EndpointConfig config) {
+
         this.session = session;
         webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
+
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
          httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
         //这里上面就是获取到的session对象
@@ -61,8 +66,86 @@ public class WebSocketTest {
          String sessionid=httpSession.getId();   //获取到用户的httpsession的ID
         jedis.set("|"+sessionid,session.getId());
         jedis.set(session.getId(),"|"+sessionid);
-        System.out.println("session的ID"+sessionid);
-        System.out.println("www"+jedis.get("o"));
+     //   System.out.println("session的ID"+sessionid);
+       // System.out.println("www"+jedis.get("o"));
+
+
+
+
+
+        int a=0;
+        boolean isblack=false;
+        if(!jedis.smembers("!"+sessionid).isEmpty()){   //判断是否还有未完成的棋局
+            Set<String> set = jedis.smembers("!"+sessionid);
+            Iterator<String> it=set.iterator() ;
+             String ehttpid= jedis.get(sessionid);
+
+            Set<String> set1 = jedis.smembers("!"+ehttpid);
+            Iterator<String> it1=set1.iterator() ;
+
+
+            if (jedis.exists("#"+sessionid)){
+                isblack=true;
+
+            }
+            while(it.hasNext()) {
+
+                String obj = it.next();
+                if(isblack){
+                    try {
+                        sendMessage("b"+obj);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+                    try {
+                        sendMessage("w"+obj);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            }
+
+            while(it1.hasNext()) {
+
+                String obj1 = it1.next();
+                if(isblack){
+                    try {
+                        sendMessage("w"+obj1);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }else{
+                    try {
+                        sendMessage("b"+obj1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+
+            }
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
         if (jedis.get("o")==null){  //判断序列中是否有人在等待
             if (jedis.get(sessionid)==null){
                 System.out.println("空的");
@@ -73,18 +156,16 @@ public class WebSocketTest {
             if (jedis.get("o")==sessionid){
 
             }else{
+                System.out.println("配对成功");
                 if (jedis.get(sessionid)==null) {
                     jedis.set(sessionid, jedis.get("o"));
                     jedis.set(jedis.get("o"), sessionid);
                     jedis.del("o");
+
                 }
             }
 
         }
-
-
-
-
 
 
 
@@ -102,7 +183,8 @@ public class WebSocketTest {
         subOnlineCount();           //在线数减1
         System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
         jedis.del(session.getId());
-       // jedis.del(httpSession.getId());
+
+
     }
 
     /**
@@ -118,9 +200,42 @@ public class WebSocketTest {
 
         String  ehttpid=jedis.get(httpid);
         String esessionid=jedis.get("|"+ehttpid);
-        System.out.println( "ehttpid"+ehttpid);
-        System.out.println(esessionid);
+        //System.out.println( "ehttpid"+ehttpid);
+       // System.out.println(esessionid);
         System.out.println(session + "来自客户端的消息:" + message);
+        Win win=new Win(httpid,ehttpid,message);
+
+        if(message.substring(0,1).equals("b")||message.substring(0,1).equals("w")){
+             message= win.cal(message);
+            //System.out.println("有一方赢了");
+        }
+        if (message.substring(0,1).equals("#")){
+             message=win.undo(message);
+        }
+        if (message.equals("@6")){
+            Set<String> set1 = jedis.smembers("!"+ehttpid);
+            Iterator<String> it1=set1.iterator() ;
+            Set<String> set2 = jedis.smembers("!"+httpid);
+            Iterator<String> it2=set2.iterator() ;
+
+
+            while(it2.hasNext()){
+                String obj=it2.next();
+                jedis.srem("!"+httpid, obj);
+
+            }
+            while(it1.hasNext()){
+                String obj1=it1.next();
+                jedis.srem("!"+ehttpid, obj1);
+
+            }
+            jedis.del("#"+httpid);
+            jedis.del("#"+ehttpid);
+            System.out.println("查看sets1集合中的所有元素:"+jedis.smembers("!"+httpid));
+            System.out.println("查看sets2集合中的所有元素:"+jedis.smembers("!"+ ehttpid));
+        }
+
+       //System.out.println("截取："+message.substring(0,1));
 
         //群发消息
         for (WebSocketTest item : webSocketSet) {
@@ -129,8 +244,10 @@ public class WebSocketTest {
 
                 System.out.println(item.session.getId());
                 if (item.session.getId().equals(esessionid)){
+                    System.out.println("敌人的ip"+item.session.getId());
                     item.sendMessage(message);
                 }
+
 
             } catch (IOException e) {
                 e.printStackTrace();
